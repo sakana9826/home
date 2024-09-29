35031470f4d718d9c606065bde5b9ae9bc95b687
xuan <sakana9826@gmail.com>
2024-09-29 13:58:52 +0800

    新增简历便签

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -44,7 +44,7 @@
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
 import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
-import { PeopleCommunity24Filled } from "@vicons/fluent"; // 注意使用正确的类别
+import { PeopleCommunity24Filled, DocumentTextClock24Filled } from "@vicons/fluent"; // 注意使用正确的类别
 import { StorageFilled } from "@vicons/material"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
@@ -73,7 +73,8 @@ const siteIcon = {
   Fire,
   LaptopCode,
   StorageFilled,
-  PeopleCommunity24Filled
+  PeopleCommunity24Filled,
+  DocumentTextClock24Filled
 };
 
 // 链接跳转
